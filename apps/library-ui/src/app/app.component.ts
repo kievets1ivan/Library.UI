ed953b8b5c5e06ccb475ac5724c9a4710155a2b1
Ivan1 <kiievets1ivan@hneu.net>
2021-04-09 18:21:13 +0300

    nrwl and pwa set up

--- apps/library-ui/src/app/app.component.ts
+++ apps/library-ui/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'library.ui-root',
+  selector: 'library-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
